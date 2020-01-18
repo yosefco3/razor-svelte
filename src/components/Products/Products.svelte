d42a83ecc62f92111194bf769cbf94a8a6c5fcbc
Yosef Cohen <yosefco12@gmail.com>
2020-01-18 22:35:58 +0200

    lesson 73

--- src/components/Products/Products.svelte
+++ src/components/Products/Products.svelte
@@ -1,15 +1,16 @@
 <script>
   import { onMount, onDestroy } from "svelte";
   import products from "../../stores/defaultProducts";
-  let localProducts = [];
+  import Product from "./Product.svelte";
 
-  onMount(() => {
-    products.subscribe(value => {
-      localProducts = value;
-    });
-  });
+  export let title = "";
 </script>
 
-{#each localProducts as product}
-  <h2>{product.title}</h2>
-{/each}
+<section class="section">
+  <h2 class="section-title">{title}</h2>
+  <div class="products-center">
+    {#each $products as product (product.id)}
+      <Product {product} />
+    {/each}
+  </div>
+</section>
--- src/stores/defaultProducts.js
+++ src/stores/defaultProducts.js
@@ -1,6 +1,15 @@
 import { writable } from 'svelte/store'
 import localProducts from '../localProducts'
 
-const store = writable([...localProducts])
+
+let flattenProducts = data => {
+    return data.map(item => {
+        item.image = item.image.url
+        return item
+    })
+}
+
+
+const store = writable([...flattenProducts(localProducts)])
 
 export default store;
