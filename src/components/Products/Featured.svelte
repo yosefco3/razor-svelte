8c2f94b11426d3f345cdfba32643e729f01710e6
Yosef Cohen <yosefco12@gmail.com>
2020-01-20 00:38:39 +0200

    lesson 74

---
+++ src/components/Products/Featured.svelte
@@ -0,0 +1,20 @@
+<script>
+  import products from "../../stores/defaultProducts";
+  import Product from "./Product.svelte";
+  import Loading from "../Loading.svelte";
+  export let title = "";
+  $: featured = $products.filter(item => item.featured);
+</script>
+
+{#if featured.length === 0}
+  <Loading />
+{:else}
+  <section class="section">
+    <h2 class="section-title">{title}</h2>
+    <div class="products-center">
+      {#each featured as product (product.id)}
+        <Product {product} />
+      {/each}
+    </div>
+  </section>
+{/if}
--- src/components/Products/Products.svelte
+++ src/components/Products/Products.svelte
@@ -1,8 +1,7 @@
 <script>
-  import { onMount, onDestroy } from "svelte";
   import products from "../../stores/defaultProducts";
   import Product from "./Product.svelte";
-
+  import Loading from "../Loading.svelte";
   export let title = "";
 </script>
 
@@ -11,6 +10,8 @@
   <div class="products-center">
     {#each $products as product (product.id)}
       <Product {product} />
+    {:else}
+      <Loading />
     {/each}
   </div>
 </section>
